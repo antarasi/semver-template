99bb4b7329abb5b5019a2f901278ede82d296ab2
Adrian <adrian.matylewicz@pollub.edu.pl>
2018-12-17 13:11:05 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemverTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("init");
+            Console.ReadLine();
+        }
+    }
+}
