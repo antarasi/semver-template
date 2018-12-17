018c1d9117cdf97429afa11984316e5ac9044a96
Adrian <adrian.matylewicz@pollub.edu.pl>
2018-12-17 13:49:09 +0100

    semver class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace SemverTemplate
 {
@@ -8,8 +9,35 @@ namespace SemverTemplate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("init");
-            Console.ReadLine();
+            if (args.Length != 3) {
+                throw new Exception(@"Usage: st.exe 1.2.3 .\template.txt .\output.txt");
+            }
+
+            var version = args[0];
+            var inputPath = args[1];
+            var outputPath = args[2];
+
+            var semVer = new SemVer(version);
+
+            using (StreamReader reader = new StreamReader(inputPath))
+            {
+                string lines = reader.ReadToEnd();
+                var output = semVer.replaceInTemplate(lines);
+
+                Console.WriteLine("Generated output:");
+                Console.WriteLine(output);
+
+                if (File.Exists(outputPath))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Output file exists and will be overwritten!");
+                }
+
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    writer.Write(output);
+                }
+            } 
         }
     }
 }
---
+++ SemVer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemverTemplate
+{
+    class SemVer
+    {
+        string[] parts;
+
+        public SemVer(string input)
+        {
+            parts = input.Split(new Char[] {'.'});
+
+            if (parts.Length != 3)
+            {
+                throw new Exception("Incorrect SemVer argument. Only x.x.x supported.");
+            }
+        }
+
+        public string replaceInTemplate(string template) {
+            return template
+                .Replace(@"${MAJOR}", parts[0])
+                .Replace(@"${MINOR}", parts[1])
+                .Replace(@"${PATCH}", parts[2]);
+        }
+    }
+}
